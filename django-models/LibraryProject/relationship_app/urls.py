c249210dae97bc918eec193e1840790b26a99ee6
Fiona Wambua <fionawambua20@gmail.com>
2025-07-19 14:21:22 +0300

    new changes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('librarian-view/', views.librarian_view, name='librarian-view'),
     path('member-view/', views.member_view, name='member-view'),
 
-    path('books/add/', views.add_book_view, name='add_book'),
-    path('books/<int:pk>/edit/', views.edit_book_view, name='edit_book'),
-    path('books/<int:pk>/delete/', views.delete_book_view, name='delete_book'),
+    path('add_book/', views.add_book_view, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book_view, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book_view, name='delete_book'),
 ]
