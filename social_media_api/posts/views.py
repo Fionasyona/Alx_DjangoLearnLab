e0b644e531936bea210ba4edaa98cddc6f8eda21
Fiona Wambua <fionawambua20@gmail.com>
2025-08-21 18:33:27 +0300

    modifications

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
 
@@ -8,17 +8,15 @@ from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
 from .pagination import DefaultResultsSetPagination
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import action
 
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
+    queryset = Post.objects.select_related("author").all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     pagination_class = DefaultResultsSetPagination
     filter_backends = [SearchFilter, OrderingFilter]
-    search_fields = ["title", "content"]  # search by title/content
+    search_fields = ["title", "content"]
     ordering_fields = ["created_at", "updated_at"]
 
     def perform_create(self, serializer):
@@ -32,16 +30,30 @@ class PostViewSet(viewsets.ModelViewSet):
         qs = post.comments.select_related("author").all()
 
         page = self.paginate_queryset(qs)
-        if page is not None:  # ✅ check pagination
+        if page is not None:
             serializer = CommentSerializer(page, many=True)
             return self.get_paginated_response(serializer.data)
 
         serializer = CommentSerializer(qs, many=True)
         return Response(serializer.data)
 
+    @action(detail=False, methods=["get"], url_path="feed", permission_classes=[IsAuthenticated])
+    def feed(self, request):
+        """Posts from users the current user follows"""
+        following_ids = request.user.following.values_list("id", flat=True)
+        qs = Post.objects.filter(author_id__in=following_ids).select_related("author").order_by("-created_at")
+
+        page = self.paginate_queryset(qs)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(qs, many=True)
+        return Response(serializer.data)
+
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
+    queryset = Comment.objects.select_related("author", "post").all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     pagination_class = DefaultResultsSetPagination
@@ -52,29 +64,3 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Ensure logged-in user is the author of the comment"""
         serializer.save(author=self.request.user)
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.select_related("author").all()
-    serializer_class = PostSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    pagination_class = DefaultResultsSetPagination
-    filter_backends = [SearchFilter, OrderingFilter]
-    search_fields = ["title", "content"]
-    ordering_fields = ["created_at", "updated_at"]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-    @action(detail=False, methods=["get"], url_path="feed", permission_classes=[IsAuthenticated])
-    def feed(self, request):
-        """Posts from users the current user follows."""
-        following_ids = request.user.following.values_list('id', flat=True)
-        qs = Post.objects.filter(author_id__in=following_ids).select_related("author").order_by("-created_at")
-
-        page = self.paginate_queryset(qs)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(qs, many=True)
-        return Response(serializer.data)        
